1782e22943df817937c63d4934f8f2d722d0165b
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-04-21 18:20:16 -0700

    Add base express server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// App Setup
+
+
+module.exports = app;
