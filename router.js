264748b215d01c4917c43e5da8986e0422735827
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-04-21 19:57:00 -0700

    set up router

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = (app) => {
-  app.get('/', (req, res, next) => {
-    res.send(['water', 'bottle']);
-  });
+  app.post('/signup', Authentication.signup);
 };
