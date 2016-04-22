e950d7edc1fa729a7770f38e42a21dd61b384c9c
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-04-21 19:01:53 -0700

    Add nodemon, use express middleware

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
+const router = require('./router');
 
 // App Setup
-
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+router(app);
 
 module.exports = app;
