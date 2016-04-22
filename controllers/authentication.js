b62ca0d95857d781d894e226c6d3ec52fa202bf8
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-04-21 22:13:55 -0700

    Add jwt login

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,20 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+/**
+ * Tokenize user object
+ *
+ * @param {user} user - user model
+ * @return {jwt} authentication token
+ */
+function tokenizeUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({
+    sub: user.id,
+    iat: timestamp
+  }, config.secret);
+}
 
 exports.signup = (req, res, next) => {
   const email = req.body.email;
@@ -28,7 +44,9 @@ exports.signup = (req, res, next) => {
 
     return user.save((saveError) => {
       if (saveError) { return next(saveError); }
-      return res.json({ success: true });
+      return res.json({
+        token: tokenizeUser(user)
+      });
     });
   });
 };
